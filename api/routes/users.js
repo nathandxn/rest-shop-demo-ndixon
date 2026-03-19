72974733c341938c9068e28d5ffa384aa7b7b125
nathandxn <nathan.dixon@snyk.io>
2026-03-19 00:04:31 -0400

    chore: adding another test comment

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-
+// adding another test comment
 router.post('/signup', (req, res, next) => {
 
     // check users to avoid duplicate emails
